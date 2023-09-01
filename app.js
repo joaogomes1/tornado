f68c74385417e9d4ff4d3a2b148a6b6bae1d4744
joaogomes1 <joao.gomes45@fatec.sp.gov.br>
2023-09-01 11:12:53 -0300

    conserta conexão à base de dados.

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const dotenv = require('dotenv');
 
 const app = express();
 
-dotenv.config({ path: './.env'})
+// environmental variables
+dotenv.config({path: './.env'})
 
+// database connection
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -13,77 +15,94 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 })
 
-// console.log('>>>', typeof(db.user));
-
 db.connect((error) => {
     if(error) {
-        // console.log(error)
+        console.log(error)
     }
     else {
         console.log("MySQL connected!")
     }
 })
 
-//
-// app.set('view engine', 'hbs')
+// view engine
+app.set('view engine', 'hbs')
 
-// //
-// const path = require("path")
+// static assets
+const path = require("path")
+const publicDir = path.join(__dirname, './public')
+app.use(express.static(publicDir))
 
-// const publicDir = path.join(__dirname, './public')
+// index routing
+app.get("/", (req, res) => {
+    res.render("index")
+})
 
-// app.use(express.static(publicDir))
+// configure port
+app.listen(5000, () => {
+    console.log("server started on port 5000")
+})
 
-// //
-// app.get("/", (req, res) => {
-//     res.render("index")
-// })
+// register
+app.get("/register", (req, res) => {
+    res.render("register")
+})
 
-// //
-// app.listen(5000, () => {
-//     console.log("server started on port 5000")
-// })
+// login
+app.get("/login", (req, res) => {
+    res.render("login")
+})
 
-// app.get("/register", (req, res) => {
-//     res.render("register")
-// })
+//// BACKEND
 
-// app.get("/login", (req, res) => {
-//     res.render("login")
-// })
+// password encryption
+const bcrypt = require("bcryptjs")
 
-// // backend
-// const bcrypt = require("bcryptjs")
 
-// app.use(express.urlencoded({extended: 'false'}))
-// app.use(express.json())
+// receive form values as json
+app.use(express.urlencoded({extended: 'false'}))
+app.use(express.json())
 
-// app.post("/auth/register", (req, res) => {
-//     const {name, email, password, password_confirm} = req.body
-//     // query
-//     db.query('SELECT email FROM users WHERE email = ?', [email], async (error, res) => {
-//         if(error) {
-//             console.log(error)
-//         }
+// retrieve user's form values
+app.post("/auth/register", (req, res) => {
+    const {name, email, password, password_confirm} = req.body
 
-//         if(result.length > 0) {
-//             return res.render('register', {
-//                 message: 'this email is already in use.'
-//             })
-//         }
-//         else {
-//             if (password !== password_confirm) {
-//                 return res.render('register', {
-//                     message: 'passwords do not match!'
-//                 })
-//             }
-//         }
+    // query
+    var result = db.query('SELECT email FROM user WHERE email = ?', [email], async (error, res) => {
+        if(error) {
+            console.log(error)
+        }
+
+        // email already exists.
+        if(result.length > 0) {
+            return res.render('register', {
+                message: 'this email is already in use.'
+            })
+        }
+        else {
+            // passwords do not match
+            if (password != password_confirm) {
+                return res.render('register', {
+                    message: 'passwords do not match!'
+                })
+            }
+        }
         
-//         let hashedPassword = await bcrypt.hash(password, 8)
+        // encrypt password
+        let hashedPassword = await bcrypt.hash(password, 8)
 
+        // feed database
+        db.query('INSERT INTO user SET?', {name: name, email: email, password: hashedPassword}, (error, res) => {
+            if(error) {
+                console.log(error)
+            } else {
+                return res.render('register', {
+                    message: 'User registered!'
+                })
+            }
+        })
 
-//     })
-// })
+    })
+})
 
     
 
